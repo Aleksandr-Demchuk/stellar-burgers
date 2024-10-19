69a6595db7c3971ca25486be5e9c4258c71b543f
Aleksandr Demchuk <aleksdemchyk@yandex.rugit config --listqgit config --listgit config --global user.email aleksdemchyk@yandex.ru>
2024-10-19 20:18:49 +0300

    Работает отображение ингридиентов. Добавлен код слайсов.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,51 +1,49 @@
+import '../../index.css';
+import styles from './app.module.css';
 import {
   ConstructorPage,
   Feed,
   ForgotPassword,
   Login,
   NotFound404,
-  ProfileOrders,
   Profile,
+  ProfileOrders,
   Register,
   ResetPassword
 } from '@pages';
-import '../../index.css';
-import styles from './app.module.css';
 
-import { AppHeader, OrderInfo, Modal } from '@components';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppHeader } from '@components';
+import { useDispatch } from '../../services/store';
+import { useEffect } from 'react';
+import { loadIngredients } from '../../services/slices/ingredientsSlice';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => (
-  // <div className={styles.app}>
-  //   <AppHeader />
-  //   <ConstructorPage />
-  // </div>
-  <BrowserRouter>
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadIngredients());
+  }, []);
+
+  return (
     <div className={styles.app}>
       <AppHeader />
-      <>
-        <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/profile/orders' element={<ProfileOrders />} />
-          <Route path='*' element={<NotFound404 />} />
-          <Route
-            path='/ingredients/:id'
-            element={<Modal title='' onClose={() => {}} />}
-          />
-          <Route
-            path='/feed/:number'
-            element={<Modal title='' onClose={() => {}} />}
-          />
-        </Routes>
-      </>
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='*' element={<NotFound404 />} />
+
+        <Route path='/feed/:number' element={<ConstructorPage />} />
+        <Route path='/ingredients/:id' element={<ConstructorPage />} />
+        <Route path='/profile/orders/:number' element={<ConstructorPage />} />
+      </Routes>
     </div>
-  </BrowserRouter>
-);
+  );
+};
 
 export default App;
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,4 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  ingredientsReducer,
+  ingredientsSliceName
+} from './slices/ingredientsSlice';
 
 import {
   TypedUseSelectorHook,
@@ -6,18 +10,16 @@ import {
   useSelector as selectorHook
 } from 'react-redux';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = combineReducers({
+  [ingredientsSliceName]: ingredientsReducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
-
 export type RootState = ReturnType<typeof rootReducer>;
-
 export type AppDispatch = typeof store.dispatch;
-
 export const useDispatch: () => AppDispatch = () => dispatchHook();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
-
 export default store;
